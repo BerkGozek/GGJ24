e1e66b0b9ba94e66eb6a6d9c21fcf94d426aa87d
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2024-01-28 14:38:34 +0300

    finish

--- GGJ24_MML/Assets/Scripts/misc/CSVRead.cs
+++ GGJ24_MML/Assets/Scripts/misc/CSVRead.cs
@@ -12,31 +12,5 @@ public class CSVRead : MonoBehaviour
     public List<string> column3 = new List<string>();
     public List<string> column4 = new List<string>();
 
-    void Start()
-    {
-        string filePath = Path.Combine(Application.dataPath, "JokeData/Jokes.csv");
-        string[] data = File.ReadAllLines(filePath);
 
-        foreach (string line in data)
-        {
-            // Regex to split on comma that is not within quotes
-            string pattern = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-            string[] fields = Regex.Split(line, pattern);
-
-            // Assuming the CSV file has at least four columns
-            if (fields.Length >= 4)
-            {
-                column1.Add(fields[0].Trim('"'));
-                column2.Add(fields[1].Trim('"'));
-                column3.Add(fields[2].Trim('"'));
-                column4.Add(fields[3].Trim('"'));
-            }
-        }
-
-        // Example to show how to use the data
-        Debug.Log("First entry in Column 1: " + column1[0]);
-        Debug.Log("First entry in Column 2: " + column2[0]);
-        Debug.Log("First entry in Column 3: " + column3[0]);
-        Debug.Log("First entry in Column 4: " + column4[0]);
-    }
 }
